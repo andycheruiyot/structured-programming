7988be73f5be64beda69034e30d26cbba5882305
andycheruiyot <kemoiandy@gmail.com>
2026-09-23 17:04:06 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     //dataType and variable
+    char userName[50];
+    printf("what is your userName!\n");
+    scanf("%s",userName);
+    printf("HELLO %s",userName);
+    return 0;
+}
